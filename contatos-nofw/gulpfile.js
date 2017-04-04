4f9eef5a7e5281f8f54a002991a0cfddcb549368
Paulo Canedo <paulocanedo@gmail.com>
2017-04-04 09:50:54 -0300

    refactoring

--- contatos-nofw/gulpfile.js
+++ contatos-nofw/gulpfile.js
@@ -6,9 +6,12 @@ var connect = require('gulp-connect');
 var util = require('gulp-util');
 
 gulp.task('src', () => {
-    gulp.src('./src/*.js')
+    gulp.src('./src/javascript/**/*.js')
         .pipe(minify())
         .pipe(gulp.dest('www/assets/js'));
+
+    gulp.src('./src/html/**/*.htm')
+        .pipe(gulp.dest('www'));
 });
 
 gulp.task('less', () => {
@@ -27,7 +30,7 @@ gulp.task('watch', () => {
     gulp.watch('./resources/less/**/*.less', [ 'less' ]);
 });
 
-gulp.task('server', function() { //mudar para o connect
+gulp.task('server', function() {
     connect.server({
         root: 'www',
         index: 'index.htm',
