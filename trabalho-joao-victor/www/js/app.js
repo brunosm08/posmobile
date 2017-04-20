edc593c6b7fdaff79e651e383b82153af6c416ff
alutsu <jovictor.miguel@gmail.com>
2017-04-20 00:24:51 -0300

    Trabalho João Victor

---
+++ trabalho-joao-victor/www/js/app.js
@@ -0,0 +1,16 @@
+let changeMenu = (evt, tela) => {
+    let i, content, menus;
+
+    content = document.getElementsByClassName("telas");
+    for(let i = 0; i < content.length; i++){
+        content[i].style.display = "none";
+    }
+
+    menus = document.getElementsByClassName("tablinks");
+    for(let i = 0; i < menus.length; i++){
+        menus[i].className = menus[i].className.replace("active","");
+    }
+
+    document.getElementById(tela).style.display = "grid";
+    evt.currentTarget.className += "active";
+}
