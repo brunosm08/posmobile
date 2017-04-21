4a3fc9b78b2d2d508f5244e59828d98610f04cca
Tito Albino Evangelista da Silva Junior <titoaesj@gmail.com>
2017-04-21 11:56:21 -0300

    Criando layout do trabalho

---
+++ trabalho-tito-junior/gulpfile.js
@@ -0,0 +1,53 @@
+let gulp = require('gulp');
+let less = require('gulp-less');
+let connect = require('gulp-connect');
+let del = require('del');
+let browserSync = require('browser-sync').create();
+
+gulp.task('server', () => {
+    connect.server({
+        root: 'www',
+        index: 'index.html',
+        liveReload: true,
+        port: 8080
+    });
+});
+
+gulp.task('assets-html', () => {
+    gulp.src('assets/html/index.html')
+        .pipe(gulp.dest('www/'));
+});
+
+gulp.task('assets-less', () => {
+    gulp.src('assets/less/style.less')
+        .pipe(less())
+        .pipe(gulp.dest('www/css'));
+});
+
+gulp.task('src', () => {
+    gulp.src('src/app.js')
+        .pipe(gulp.dest('www/js'));
+});
+
+gulp.task('clean:www', function () {
+    return del.sync('www');
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'www'
+        },
+    })
+});
+
+gulp.task('watch', ['browserSync'], () => {
+    gulp.watch('assets/html/*.html', ['assets-html']);
+    gulp.watch('assets/less/*.less', ['assets-less']);
+    gulp.watch('src/*.js', ['src']);
+    gulp.watch('assets/html/*.html', browserSync.reload);
+    gulp.watch('assets/less/*.less', browserSync.reload);
+    gulp.watch('src/*.js', browserSync.reload);
+});
+
+gulp.task('default', ['assets-html', 'assets-less', 'src']);
