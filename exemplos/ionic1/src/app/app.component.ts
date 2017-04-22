f38394753fb3ede3bfc2b5419e019395c6436d32
Paulo Canedo <paulocanedo@gmail.com>
2017-04-22 18:09:46 -0300

    aplicacao de exemplo ionic

---
+++ exemplos/ionic1/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage:any = HomePage;
+
+  pages:Array<{titulo:string,component:Component}>;
+
+  constructor(public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+  ) {
+    this.init();
+
+    this.pages = [
+      {titulo: 'Pagina inicial', component: HomePage},
+      {titulo: 'Sobre', component: AboutPage}
+    ];
+  }
+
+  init() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  abrirPagina(pagina) {
+    this.nav.setRoot(pagina.component);
+  }
+}
